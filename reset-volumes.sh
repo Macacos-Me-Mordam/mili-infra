e09cc98b27fe4343cdf98c9ec7cb01aadd5e5c81
luiGarcg <luisggcosta2006@gmail.com>
2025-07-30 09:37:00 -0300

    fix the container settings

--- reset-volumes.sh
+++ reset-volumes.sh
@@ -1,10 +1,16 @@
 docker compose down
-docker volume rm $(docker volume ls -q)
 
+# Remove só o volume deste projeto, se for nomeado
+docker volume rm postgres_data
+
+# Opcional: salvar realm
 cp ./keycloak/mili-realm.json ./mili-realm.json
 
+# Limpar pastas locais
 rm -rf ./keycloak
-sudo rm -rf ./postgres_data
+rm -rf ./postgres_data # só se existir localmente
 
+# Recriar keycloak dir com realm
 mkdir keycloak
 mv ./mili-realm.json ./keycloak/mili-realm.json
+cc      
